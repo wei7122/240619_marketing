845bec7bd3fd244377718cda0582054b159c8cdd
wei <124412877+wei7122@users.noreply.github.com>
2024-06-18 18:02:37 +0800

    Add files via upload

---
+++ data-tab.js
@@ -0,0 +1,203 @@
+// 需要複製請連著"{},"整個區塊複製
+// 是價格的話請用price，會加上$符號，不是價格請用'text'，不會加上$符號
+// 區塊：大牌抗暑對策
+var dataTabSlider = [
+  // 1
+  [ 
+    {
+      productNumber: 7298898, 
+      name: '【西雅圖】極品綜合濾掛(藍山風味)8g/10入/盒-網  ',
+      price: 12,
+    },
+    {
+      productNumber: 4091795, 
+      name: '《皮克敏™３ 豪華版》盆栽飾品',
+      price: 12,
+    },
+    {
+      productNumber: 4251546, 
+      name: '即期【UCC】AROMA BREW艾洛瑪紅茶拿鐵253ml',
+      price: 12,
+    },
+    {
+      productNumber: 7298842, 
+      name: '任-QIDINA 冬季聖誕暖心精油擴香瓶*1+紙袋*1',
+      price: 12,
+    },
+    {
+      productNumber: 7296783, 
+      name: '【維諾妮卡】嚴選Q彈家居拖鞋-專利 可機洗 水洗 台灣製造 推薦 居家 防水拖鞋 室內拖鞋 室內鞋',
+      price: 12,
+    },
+    {
+      productNumber: 100000162601, 
+      name: '防水牛津布休閑單肩女包 ',
+      price: 12,
+    },
+  ],
+  // 2
+  [ 
+    {
+      productNumber: 4150481, 
+      name: '【毛寶兔】超天然2倍濃縮小蘇打植物抗菌洗衣精(1000g)',
+      price: 122,
+    },
+    {
+      productNumber: 4582668, 
+      name: 'aibo 360度折疊伸縮手機支架(IP-MA34)',
+      price: 122,
+    },
+    {
+      productNumber: 2899580, 
+      name: 'FU TOGO森沐環保餐具吸管五件組',
+      price: 122,
+    },
+    {
+      productNumber: 7237897, 
+      name: 'Parfum 巴黎帕芬 香水護髮油摩洛哥髮油 隨身組 10mlx2入(小蒼蘭/鳶尾花/玫瑰3款隨機出貨)',
+      price: 122,
+    },
+    {
+      productNumber: 3615616, 
+      name: '任-【匠心】醫用級 婦幼平面口罩(顏色任選一)-50入',
+      price: 122,
+    },
+    {
+      productNumber: 100001659635, 
+      name: '2023新款馬里奧問號led燈',
+      price: 122,
+    },
+  ],
+  // 3
+  [ 
+    {
+      productNumber: 7300469, 
+      name: 'adidas 休閒鞋 女鞋 小白鞋 運動鞋 多款 單一價 愛迪達 ',
+      price: 1212,
+    },
+    {
+      productNumber: 7187963, 
+      name: 'Skinarma 5000mAh 20W潮流支架款磁吸行動電源',
+      price: 1212,
+    },
+    {
+      productNumber: 2503739, 
+      name: '東森獨家★【SAMPO 聲寶】4D水洗三刀頭電動刮鬍刀 EA-Z1904WL(電鬍刀/修容刀)',
+      price: 1212,
+    },
+    {
+      productNumber: 4690413, 
+      name: 'Estee Lauder 特潤全能修護亮眼霜15ml(公司貨)',
+      price: 1212,
+    },
+    {
+      productNumber: 3591145, 
+      name: 'WEDAR 雙銀獎精萃蔓越莓嚼錠私密6盒組(30顆/盒)-網',
+      price: 1212,
+    },
+    {
+      productNumber: 100002027519, 
+      name: '24寸萬向輪耐用登機旅行箱鋁框',
+      price: 1212,
+    },
+  ],
+  // 4
+  [ 
+    {
+      productNumber: 7298898, 
+      name: '【西雅圖】極品綜合濾掛(藍山風味)8g/10入/盒-網  ',
+      price: 12,
+    },
+    {
+      productNumber: 4091795, 
+      name: '《皮克敏™３ 豪華版》盆栽飾品',
+      price: 12,
+    },
+    {
+      productNumber: 4251546, 
+      name: '即期【UCC】AROMA BREW艾洛瑪紅茶拿鐵253ml',
+      price: 12,
+    },
+    {
+      productNumber: 7298842, 
+      name: '任-QIDINA 冬季聖誕暖心精油擴香瓶*1+紙袋*1',
+      price: 12,
+    },
+    {
+      productNumber: 7296783, 
+      name: '【維諾妮卡】嚴選Q彈家居拖鞋-專利 可機洗 水洗 台灣製造 推薦 居家 防水拖鞋 室內拖鞋 室內鞋',
+      price: 12,
+    },
+    {
+      productNumber: 100000162601, 
+      name: '防水牛津布休閑單肩女包 ',
+      price: 12,
+    },
+  ],
+  // 5
+  [ 
+    {
+      productNumber: 4150481, 
+      name: '【毛寶兔】超天然2倍濃縮小蘇打植物抗菌洗衣精(1000g)',
+      price: 122,
+    },
+    {
+      productNumber: 4582668, 
+      name: 'aibo 360度折疊伸縮手機支架(IP-MA34)',
+      price: 122,
+    },
+    {
+      productNumber: 2899580, 
+      name: 'FU TOGO森沐環保餐具吸管五件組',
+      price: 122,
+    },
+    {
+      productNumber: 7237897, 
+      name: 'Parfum 巴黎帕芬 香水護髮油摩洛哥髮油 隨身組 10mlx2入(小蒼蘭/鳶尾花/玫瑰3款隨機出貨)',
+      price: 122,
+    },
+    {
+      productNumber: 3615616, 
+      name: '任-【匠心】醫用級 婦幼平面口罩(顏色任選一)-50入',
+      price: 122,
+    },
+    {
+      productNumber: 100001659635, 
+      name: '2023新款馬里奧問號led燈',
+      price: 122,
+    },
+  ],
+  // 6
+  [ 
+    {
+      productNumber: 7300469, 
+      name: 'adidas 休閒鞋 女鞋 小白鞋 運動鞋 多款 單一價 愛迪達 ',
+      price: 1212,
+    },
+    {
+      productNumber: 7187963, 
+      name: 'Skinarma 5000mAh 20W潮流支架款磁吸行動電源',
+      price: 1212,
+    },
+    {
+      productNumber: 2503739, 
+      name: '東森獨家★【SAMPO 聲寶】4D水洗三刀頭電動刮鬍刀 EA-Z1904WL(電鬍刀/修容刀)',
+      price: 1212,
+    },
+    {
+      productNumber: 4690413, 
+      name: 'Estee Lauder 特潤全能修護亮眼霜15ml(公司貨)',
+      price: 1212,
+    },
+    {
+      productNumber: 3591145, 
+      name: 'WEDAR 雙銀獎精萃蔓越莓嚼錠私密6盒組(30顆/盒)-網',
+      price: 1212,
+    },
+    {
+      productNumber: 100002027519, 
+      name: '24寸萬向輪耐用登機旅行箱鋁框',
+      price: 1212,
+    },
+  ],
+];
